a840a9e804fce35cd804ac49319564f47bb1c67d
dutchmims <jameshealy901@gmail.com>
2023-05-04 21:04:49 +0000

    Initial commit with my print dlay function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import time
+
+"""
+add print_delay function to create a more immersive experience
+"""
+
+def print_delay(text, delay=1.5):
+    print(text)
+    time.sleep(delay)
+
