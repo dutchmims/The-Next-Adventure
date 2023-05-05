dbd4af285d235c10d01fee6dc722811693f3598e
dutchmims <jameshealy901@gmail.com>
2023-05-05 09:36:52 +0000

    Correcting an issue with the time delay

--- run.py
+++ run.py
@@ -8,9 +8,10 @@ import time
 add print_delay function to create a more immersive experience
 """
 
-def print_delay(text, delay=1.5):
+def print_delay(text, delay=2):
     print(text)
     time.sleep(delay)
+
 
 def title():
     print("======================================")
@@ -19,35 +20,28 @@ def title():
     print("                                      ")
     print("======================================")
 
+
 def instructions():
     print_delay("Instructions:")
     print_delay("You wake up in a strange and mysteriuos room, confused and scared.")
+
     print_delay("You get yourself together and decide to go and explore.")
+
     print_delay("You have to choose between different doors, rooms and path to find your way.")
+
     print_delay("Your task is to get to the end and win the game.")
+
     print_delay("Be careful though! The decisions you make will be your fate.")
+
     print_delay("======================================")
 
 def intro():
     print_delay("You wake up in a mysterious room, confused and scared, without any memory of how you goy there!")
+
     print_delay("There are three doors. The right door could save yout life, the wrong door could end it!")
+
     print_delay("There are 3 doors, a red door, a blue door, and a green door.")
 
-def choose_door():
-    print_delay("Which door do you choose? (red/blue/green)")
-    choice = input().lower()
-    if choice == "red":
-        print_delay("You open the red door.")
-        red_door()
-    elif choice == "blue":
-        print_delay("You open the blue door.")
-        blue_door()
-    elif choice == "green":
-        print_delay("You open the green door.")
-        green_door()
-    else:
-        print_delay("Invalid choice. Try again.")
-        choose_door()
 
 def choose_door():
     print_delay("Which door do you choose? (red/blue/green)")
@@ -65,6 +59,46 @@ def choose_door():
         print_delay("Invalid choice. Try again.")
         choose_door()
 
+
+def choose_door():
+    print_delay("Which door do you choose? (red/blue/green)")
+    choice = input().lower()
+    if choice == "red":
+        print_delay("You open the red door.")
+        red_door()
+    elif choice == "blue":
+        print_delay("You open the blue door.")
+        blue_door()
+    elif choice == "green":
+        print_delay("You open the green door.")
+        green_door()
+    else:
+        print_delay("Invalid choice. Try again.")
+        choose_door()
+
+
+def red_door():
+    print_delay("You enter a room filled with treasure!")
+    print_delay("Congratulations! You win the game.")
+
+def blue_door():
+    print_delay("You enter a room full of beasts.")
+    print_delay("The beasts devour you.")
+    print_delay("Game Over!")
+
+def green_door():
+    print_delay("You enter a dark forest.")
+    print_delay("There are two paths in front of you.")
+    print_delay("One leads to a cave and the other to a river.")
+
+    print_delay("Which path do you choose? (cave/river)")
+    choice = input().lower()
+    if choice == "cave":
+        print_delay("You enter a cave and find a hidden treasure!")
+        print_delay("Congratulations! You win the game.")
+    elif choice == "river":
+        print_delay("You follow the river and encounter a group of hostile creatures.")
+        fight_or_flee()
 
 
 def play_game():
