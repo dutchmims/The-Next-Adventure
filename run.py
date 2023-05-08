4226a190e28ba872ca142f1881efef8f054d0d15
dutchmims <jameshealy901@gmail.com>
2023-05-08 21:17:58 +0000

    Add more story, more choices, more decisons

--- run.py
+++ run.py
@@ -81,7 +81,6 @@ def black_door():
         print_delay("You have to decide to fight your way out or try and sneak by the Trolls")
 
         print_delay("Which way will you choose? (fight/sneak)")
-
     elif choice == "right":
         print_delay("The noise of people coming towards gets louder and louder")
         print_delay("you try to find somewher to hide, but its too late")
@@ -91,7 +90,6 @@ def black_door():
         print_delay("You are brought back to the castle and locked up in the dungeon")
         print_delay("You have no way of escape! You shall remain here until death takes you")
         print_delay("Game Over!")
-
     else:
         print_delay("Invalid choice. Try again.")
         choose_door()
@@ -101,13 +99,11 @@ def black_door():
         print_delay("You decide to take on the two Trolls! Attacking them while their backs are turned.") 
         print_delay("you surprise them and get the upper hand. After a long struggle, the Trolls gain the upper hand and kill you")
         print_delay("Game Over!")
-
     elif choice == "sneak":
         print_delay("You manage to climb the wall of the maze and find a way out without the Trolls spotting you")
         print_delay("There are two paths in front of you")
         print_delay("One leads to a cave and the other to a river.")
         print_delay("Which path do you choose? cave/river")
-
     else:
         print_delay("Invalid choice. Try again.")
         black_door()	
@@ -130,34 +126,75 @@ def black_door():
 
 
 def yellow_door():
-    print_delay("You enter a room full of beasts.")
-    print_delay("The beasts devour you.")
-    print_delay("Game Over!")
-    print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
-print_delay("Game Over!")
+    print_delay("You open the door to darkness")
+    print_delay("Scared and alone, you cant see past your nose")
+    print_delay("You scuffle along the wall until you feel a draft")
+    print_delay("Still unable to see anything, you keep going towards the draft ")
+    print_delay("Eventually, you come across a secret passage")
+    print_delay("Do you go down the secret passage or continue to search the room?")
+  
+    print_delay("Which way will you choose? passage/room")
+    choice = input().lower()
+    if choice == "passage":
+        print_delay("You head down the passage")
+        print_delay("Its dark and damp with a heavy stench that nearly overpowers you")
+        print_delay("You move down the passage extremely slowly, wondering where it goes and what is ahead of you")
+        print_delay("You see a light! Its getting brighter and brighter the further you go")
+        print_delay("Eventually you seen to have run out of passage and peer out what appears to be, the side of a mountain!")
+        print_delay("Confused and exhausted, with nowhere to go expect back from where you came from, you stop to take breath and think")
+        print_delay("All of a sudden, you hear a distant noise, and a breeze goes past you")
+        print_delay("The noise gets lounder and louder, the breeze has now turned to a strong wind, passing you while you hang on to the sides of the passage!")
+        print_delay("You start to panic!")
+        print_delay("Then, a torrent of water barrels down the passage and sweeps you out the mountain, to certain death")
+        print_delay("Game Over!")
+    elif choice == "room":
+        print_delay("You decide to stay in the room and find out what secrets are hidden inside")
+        print_delay("After shuffling past the passage you trip over something on the ground!")
+        print_delay("Still unable to see past you nose, you bend down to investigate only to find a pile of bones!")
+        print_delay("Terrified and still unable to see anything, you hear something!")
+        print_delay("The smell in the room becomes stronger!")
+        print_delay("You hear something that resembles yawning")
 
+        print_delay("Before you know it, growling takes over, then snarling")
+        print_delay("Terrified, you crouch down and wait your doom!")
+        print_delay("Then out of nowhere, a pack of savage wolves pounce and tear you apart")
+        print_delay("Game Over!")
+        fight_or_flee()
+    else:
+        print_delay("Invalid choice. Try again.")
+        yellow_door()
 
 
 def white_door():
-    print_delay("You enter a dark forest.")
-    print_delay("There are two paths in front of you.")
+    print_delay("You find yourself in an overgrown, dark and damp forest with a thick fog all around.")
+    print_delay("Not knowing where you are or being able to see the sun, gives you a tough choice to make")
+    print_delay("You decide to walk straight ahead, with caution, avoiding deadly sharp and spikey bushes, watching your back")
+    print_delay("After a while without gaining to much distance, you decide to rest, gather your bearings before moving forward")
+    print_delay("You give yourself the lift needed to carry on, even with no sign of anything to give you a sense of optimism")
+    print_delay("Eventually you come across two paths")
     print_delay("One leads to a cave and the other to a river.")
 
     print_delay("Which path do you choose? (cave/river)")
     choice = input().lower()
     if choice == "cave":
-        print_delay("You enter a cave and find a hidden treasure!")
-        print_delay("Congratulations! You win the game.")
+        print_delay("You head in to the cave")
+        print_delay("It's late, so you decide to collect any wood you can find and light a fire to stay warm")
+        print_delay("Your fire warms up the cave and you fall asleep")
+        print_delay("Unfortunately a bear lives deep inside this cave and kills you in the middle of the night")
+        print_delay("You are never seen or heard from again")
+        print_delay("Game over.")
+    
     elif choice == "river":
-        print_delay("You follow the river and encounter a group of hostile creatures.")
+        print_delay("You follow the river for several hours until you come across a rope bridge")
+        print_delay("It's very old and on its last legs! Crossing it will be treacherous")
+        print_delay("You decide to use the rope bridge and cross the river at this point")
+        print_delay("Once across you continue to follow the river and suddenly you come upon a settlement.")
+        print_delay("Several natives spot you approaching!")
         fight_or_flee()
-
+    else:
+        print_delay("Invalid choice. Try again.")
+        white_door()
+       
 
 def fight_or_flee():
     print_delay("Do you want to fight or flee? (fight/flee)")
@@ -267,8 +304,7 @@ def temple():
     else:
         print_delay("Invalid choice. Try again.")
         temple()
-
-
+        
 def play_game():
     title()
     instructions()
